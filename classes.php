d467df9f7516f9cb624081f76887307443db0bfa
Mahesh Asolkar <mahesh@mahesha.com>
2010-02-26 22:30:32 -0800

    Initial population

---
+++ classes.php
@@ -0,0 +1,123 @@
+<?php
+//  Copyright 2008 Mahesh Asolkar
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+//
+// Classes used in this project
+//
+class queue {
+  var $__config, $__status;
+  var $items = array ();
+
+  function queue (&$__config, &$__status) {
+    $this->__config = $__config;
+    $this->__status = $__status;
+
+    $this->__status['raw_queue_text'] = file_get_contents ($this->__config['queue_file']);
+
+    $raw_array = preg_split (
+                  '/^' . $this->__config['queue_separator'] . '$/ms',
+                  $this->__status['raw_queue_text']);
+
+    $id = 1;
+    foreach ($raw_array as $item) {
+      if (preg_match('/^\s*$/', $item)) {
+        // skip empties
+      } else {
+        $item_obj = new queue_item ($id, $item);
+        array_push ($this->items, $item_obj);
+        $id ++;
+      }
+    }
+  }
+
+  function pretty_print () {
+    $pretty_txt =  "<div class='queue_item_list'>\n";
+    foreach ($this->items as $item_obj) {
+      $pretty_txt .= $item_obj->pretty_print();
+    }
+    $pretty_txt .= "</div> <!-- queue_item_list -->\n";
+
+    return $pretty_txt;
+  }
+}
+
+class queue_item {
+  var $id;
+  var $raw_text;
+  var $tags = array ();
+  var $body = '';
+  var $pretty_text = '';
+
+  function queue_item ($id, $raw_text) {
+    $this->id = $id;
+    $this->raw_text = $raw_text;
+    $this->parse_raw_text ();
+  }
+
+  function tag_wrap ($tag) {
+      $tag_class = preg_replace ('/\W/', '_', $tag);
+      return "<div class='queue_item_tag tc_$tag_class'>$tag</div>";
+  }
+
+  function parse_raw_text () {
+    //
+    // Clean up the raw text by stripping white space
+    //
+    $this->raw_text = preg_replace ('/^\s+/', '', $this->raw_text);
+    $this->raw_text = preg_replace ('/\s+$/', '', $this->raw_text);
+
+    //
+    // Get tags from the 'Tags:' line
+    //
+    preg_match ('/^Tags\s*:\s*(.*)$/m', $this->raw_text, $raw_tags);
+    $this->tags = preg_split ('/\s*,\s*/', $raw_tags[1]);
+
+    $pretty_tags = "<div class='queue_item_tag_wrap'>"
+                    . implode (" ", array_map (array($this, 'tag_wrap'), $this->tags))
+                    . "</div>";
+
+    //
+    // Now nuke the 'Tags:' line, and clean up again
+    //
+    $this->raw_text = preg_replace ('/^Tags\s*:.*$/m', '', $this->raw_text);
+    $this->raw_text = preg_replace ('/^\s+/', '', $this->raw_text);
+    $this->raw_text = preg_replace ('/\s+$/', '', $this->raw_text);
+
+    $tag_classes = '';
+    foreach ($this->tags as $tag) {
+      $tag = preg_replace ('/\W/', '_', $tag);
+      $tag_classes .= ' tc_' . $tag;
+    }
+
+    $this->pretty_text = <<<EOD
+  <div class='queue_item {$tag_classes}' id='item_{$this->id}'>
+    <div class='queue_item_head'>
+      <div class='queue_item_id'>Item {$this->id}</div>
+      {$pretty_tags}
+      <br class='separator'>
+    </div> <!-- queue_item_head -->
+    <div class='queue_item_body'>
+      <pre>$this->raw_text</pre>
+    </div> <!-- queue_item_body -->
+  </div> <!-- queue_item -->
+EOD;
+  }
+
+  function pretty_print () {
+    return $this->pretty_text;
+  }
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,107 @@
+<?php
+//  Copyright 2008 Mahesh Asolkar
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+//
+// Include library of functions
+//
+include 'classes.php';
+
+$__config['queue_file'] = 'Sata.queue';
+$__config['queue_separator'] = '------ Item ------';
+
+$__status['page_title'] = 'My Task Queue';
+$__status['raw_queue_text'] = '';
+
+$__queue = new queue($__config, $__status);
+
+//
+// Functions
+//
+function http_doc_type() {
+  echo '<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
+}
+
+?>
+
+<?php http_doc_type() ?>
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="StyleSheet" href="style.css" type="text/css" title="Design Style">
+<title><?php echo "{$__status['page_title']}" ?></title>
+
+</head>
+<body>
+<div id='shrink_wrapper_shell'>
+<div id='shrink_wrapper'>
+<h1>My Task Queue</h1>
+<div id="show_all_botton">Show All</div>
+<div id="collapse_all_botton">Collapse All</div>
+<?php
+
+echo $__queue->pretty_print();
+
+?>
+
+</div> <!-- shrink_wrapper -->
+</div> <!-- shrink_wrapper_shell -->
+</body>
+
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.4.2.min.js"></script>
+<script type="text/javascript">
+//<![CDATA[
+//
+// Load JS after everything is in place
+//
+
+//
+// jQuery stuff
+//
+$(document).ready(function(){
+  $(".queue_item_tag").click (function(){
+    var class_list = $(this).attr('class');
+    var selected_class = class_list.substr(class_list.indexOf("tc_"));
+
+    $(".queue_item").addClass('hide_queue_item');
+    $(".queue_item." + selected_class).removeClass('hide_queue_item');
+  }),
+  $("#show_all_botton").click (function(){
+    $(".queue_item").removeClass('hide_queue_item');
+    // $(".queue_item_body").removeClass('hide_queue_item_body');
+  }),
+  $("#collapse_all_botton").click (function(){
+    $(".queue_item_body").addClass('hide_queue_item_body');
+  }),
+  $(".queue_item_head").click (function(){
+    if ($(this).siblings().hasClass('hide_queue_item_body')) {
+      $(this).siblings().removeClass('hide_queue_item_body');
+    } else {
+      $(this).siblings().addClass('hide_queue_item_body');
+    }
+  })
+});
+
+
+//]]>
+</script>
+
+</html>
+
+<?php
+
+?>
